ae3135369725fd860e9201e55979de5e0b7dffe2
Your Name <you@example.com>
2023-11-24 16:44:57 +0700

    v1

---
+++ main.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//     $('.owl-carousel').owlCarousel({
+//         loop:true,
+//         items: 12,
+//         margin: 10,
+//         touchDrag: true,
+//         dots: false,
+//         autoWidth: true,
+//         responsiveClass: true,
+//         responsive: {
+//             0: {
+//                 items: 1,
+//                 nav: true
+//             },
+//             600: {
+//                 items: 3,
+//                 nav: false
+//             },
+//             1000: {
+//                 items: 5,
+//                 nav: true,
+//                 loop: false
+//             }
+//         }
+//     })
+// });
